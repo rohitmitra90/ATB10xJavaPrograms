5b704fddfca5fe7b9befebe9692fe0432181f6e0
rohitmitra90 <rohit231090mitra@yahoo.com>
2025-01-05 19:42:00 +0530

    Commit

---
+++ Summation.java
@@ -0,0 +1,13 @@
+package Prime;
+
+import java.util.Scanner;
+
+public class Summation {
+    public static void main(String[] args) {
+        System.out.println("enter a number");
+        Scanner scanner =new Scanner(System.in);
+        int a = scanner.nextInt();
+        int sum = 10+a;
+        System.out.println("Sum is : "+sum);
+    }
+}
